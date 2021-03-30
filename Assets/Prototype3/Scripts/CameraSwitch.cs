247ac13ea76aab74c216bb3e7f8de725991f1192
leena54 <leenacham2002@gmail.com>
2021-03-30 21:03:17 +1100

    Prototype 3
    - rotating game
    - rotate the shape so that it fits in the hole

---
+++ Assets/Prototype3/Scripts/CameraSwitch.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera frontCam;
+    public Camera backCam;
+
+    void Start()
+    {
+        frontCam.enabled = true;
+        backCam.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            frontCam.enabled = !frontCam.enabled;
+            backCam.enabled = !backCam.enabled;
+        }
+    }
+}
---
+++ Assets/Prototype3/Scripts/Rotate.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Rotate : MonoBehaviour
+{
+    float rotSpeed = 40;
+    public GameObject shape;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private float distance = 5f;
+    private float lerpTime = 5;
+    private float currentLerpTime;
+    private bool keyHit = false;
+
+
+    void Start()
+    {
+        startPos = shape.transform.position;
+        endPos = shape.transform.position + Vector3.forward * distance;
+    }
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.up, -rotX);
+        transform.Rotate(Vector3.right, rotY);
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            keyHit = true;
+        }
+
+        if (keyHit == true)
+        {
+            currentLerpTime += Time.deltaTime;
+            if(currentLerpTime >= lerpTime)
+            {
+                currentLerpTime = lerpTime;
+            }
+
+            float Perc = currentLerpTime / lerpTime;
+            shape.transform.position = Vector3.Lerp(startPos, endPos, Perc);
+        }
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == ("Wall"))
+        {
+            keyHit = false;
+            shape.transform.position = new Vector3(0, 0, -1);
+        }
+    }
+
+}
